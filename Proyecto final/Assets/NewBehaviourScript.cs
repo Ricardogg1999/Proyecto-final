f30570a87776c804e11b8e0289e29e017bdd8a61
Ricardogg1999 <71872702+Ricardogg1999@users.noreply.github.com>
2021-04-23 11:45:07 +0100

    Update NewBehaviourScript.cs
    modifique la primera prueba

--- Proyecto final/Assets/NewBehaviourScript.cs
+++ Proyecto final/Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("si");
+        Debug.Log("si pero no ");
     }
 
     // Update is called once per frame
