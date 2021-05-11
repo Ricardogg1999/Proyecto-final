3e91f4efb93a405d15afd85beeeee36b71e77da5
Ricardo <ricardo.gg9965@gmail.com>
2021-05-11 09:46:00 +0100

    Terminado mapa 3d, Terminado enemigos aleatorios

---
+++ Proyecto final/Assets/Script/Salida.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Salida : MonoBehaviour
+{
+    public GameObject[] Zombie;
+    private int currentIndex = 0;
+    public float Tiempo = 0f;
+    public float repetir = 10f;
+
+
+    private void Update()
+    {
+        Tiempo += Time.deltaTime;
+        if (Tiempo >= repetir)
+        {
+            int newnewIndex = Random.Range(0, Zombie.Length);
+            Zombie[currentIndex].SetActive(false);
+            currentIndex = newnewIndex;
+            Zombie[currentIndex].SetActive(true);
+            Tiempo -= repetir;
+        }
+    }
+}
