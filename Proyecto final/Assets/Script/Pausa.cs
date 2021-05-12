2fa7ab14dcf34935dfdf086edf5e1aba3e3f3e52
Ricardo <ricardo.gg9965@gmail.com>
2021-05-12 11:49:14 +0100

    Terminado mapa 3d y musica

---
+++ Proyecto final/Assets/Script/Pausa.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pausa : MonoBehaviour
+{
+    public GameObject PauseUI;
+    public bool Pausado = false;
+    public GameObject Boton;
+
+    void Start()
+    {
+        PauseUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Boton"))
+        {
+            Pausado = !Pausado;
+        }
+        
+        if (Pausado)
+        {
+            PauseUI.SetActive(true);
+            Boton.SetActive(false);
+            Time.timeScale = 0;
+        }
+        if (!Pausado)
+        {
+            PauseUI.SetActive(false);
+            Boton.SetActive(true);
+            Time.timeScale = 1;
+        }
+
+    }
+} 
+
--- Proyecto final/Assets/Script/Salida.cs
+++ Proyecto final/Assets/Script/Salida.cs
@@ -2,24 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Salida : MonoBehaviour
 {
     public GameObject[] Zombie;
     private int currentIndex = 0;
     public float Tiempo = 0f;
     public float repetir = 10f;
-
+    public AudioSource grito;
+    public bool cementerio;
 
     private void Update()
     {
         Tiempo += Time.deltaTime;
         if (Tiempo >= repetir)
         {
+
             int newnewIndex = Random.Range(0, Zombie.Length);
+            grito.Play();
             Zombie[currentIndex].SetActive(false);
             currentIndex = newnewIndex;
             Zombie[currentIndex].SetActive(true);
             Tiempo -= repetir;
         }
+
+        
     }
+   
 }
