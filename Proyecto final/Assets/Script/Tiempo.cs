9edf96817d85f7806f51a0314b5aafd9b74119af
Ricardo <ricardo.gg9965@gmail.com>
2021-05-17 09:53:18 +0100

    Terminado animaciones de armas , cronometro y modelos

---
+++ Proyecto final/Assets/Script/Tiempo.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Tiempo : MonoBehaviour
+{
+    public float timedown = 10f;
+    private float remaingTime = 0.0f;
+    public TextMeshProUGUI Contador;
+    public bool isruning = false;
+    public TextMeshProUGUI Decimas;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reinicio();
+    }
+    void Update()
+    {
+        
+        if (isruning == true)
+        {
+            if (timedown >= 0)
+            {
+                timedown -= Time.deltaTime;
+                remaingTime = timedown;
+            }
+
+
+        }
+        int RestoSegundos = (int)timedown % 3600;
+        int segundos = RestoSegundos % 60;
+        float decimas = (timedown - (int)timedown) * 100;
+        Decimas.text = decimas.ToString("00");
+        Contador.text =segundos.ToString("00");
+
+
+    }
+    public void Reinicio()
+    {
+        remaingTime = timedown;
+    }
+
+    
+}
